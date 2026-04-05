bf920844420162f16d7f7df023ab3a3087953b64
Astro <hunterjmp@gmail.com>
2026-04-05 16:52:01 -0400

    syntax fixes for ChatClient program

--- ChatClient.java
+++ ChatClient.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-class ChatClient extends Thread{
+public class ChatClient extends Thread{
     public static void main(String argv[]) throws Exception {
         //parse arguments to variables
         String server = argv[0];
@@ -10,9 +10,9 @@ class ChatClient extends Thread{
         
         Socket connection = new Socket(server,port);
         //create reader for user input, path to output user input to server, and path for messages from server
-        userOutput = new DataOutputStream(connection.getOutputStream());
-        userInput = new BufferedReader (new InputStreamReader(System.in));
-        serverOutput = new DataInputStream(connection.getInputStream());
+        DataOutputStream userOutput = new DataOutputStream(connection.getOutputStream());
+        BufferedReader userInput = new BufferedReader (new InputStreamReader(System.in));
+        DataInputStream serverOutput = new DataInputStream(connection.getInputStream());
 
         ChatClient readingThread = new ChatClient();
         readingThread.start();
